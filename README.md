4357a12fbb520b9024ae7f8fbce3c7f6a5339115
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-10-01 19:00:48 +0530

    Add Docker

---
+++ README.md
@@ -0,0 +1,6 @@
+# Analytics API using FastAPI + Time—series Postgres
+
+## Docker
+
+- `docker build -t analytics-api -f Dockerfile .`
+- `docker run analytics-api`
---
+++ boot/docker-run.sh
@@ -0,0 +1,7 @@
+source /opt/venv/bin/activate
+
+cd /code
+RUN_PORT=${PORT:-8000}
+RUN_HOST=${HOST:-0.0.0.0}
+
+gunicorn -k uvicorn.workers.UvicornWorker -b $RUN_HOST:RUN_PORT src.main:app
